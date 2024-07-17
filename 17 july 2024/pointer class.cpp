00291daaaf4cf9a942a1d0da93ef1690b08210ba
Abhay Dixit <dixitshivam534@gmail.com>
2024-07-17 22:51:09 +0530

    pointer intro

---
+++ 17 july 2024/pointer class.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int num1,num2;
+      cout<<"enter two numbers:";
+   cin>>num1>>num2;
+   int *p=&num1;
+   int *p2=&num2;
+
+   
+//   cout<<"Address of num1:"<<p<<endl;
+//   int *p1=new int;  //block heap section
+//       *p=900;             //new--> address return
+                    // address return holds in p1
+   
+   if(*p>*p2){
+   	cout<<*p<<" is greater than "<<*p2;
+   }
+   else{
+   	cout<<*p2<<" is greater than "<<*p;
+   }
+}
+
