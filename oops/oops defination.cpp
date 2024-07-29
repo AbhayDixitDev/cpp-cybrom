4b924d1cc66093a7390dcaf02312fe5f6ab0194d
Abhay Dixit <dixitshivam534@gmail.com>
2024-07-29 12:25:49 +0530

    oops definition

---
+++ oops/oops defination.cpp
@@ -0,0 +1,35 @@
+-------------------------------------------------------------------------------------------------------------
+Class and Object:
+	In c++, to solve real world problem we have two user defined data type  structure and class.	
+	real world problem.(tangible and non tangible)
+	IRCTC --> Ticket , food ,passenger , money	
+-------------------------------------------------------------------------------------------------------------	
+structure and class 
+	here we define attribute (data) and behavior (method)
+	but we want to secure our data  , in c++ we generally use class because in structure we dont 
+	have access specifier, (it is a concept of 	object oriented )	
+note:
+	structure member + function: public
+	class member + function : private (by default)
+-------------------------------------------------------------------------------------------------------------		
+Object Defination:
+    objects are real world entity,	
+    objects are the instance of the class
+    objects are also known as class variable.
+--------------------------------------------------------------------------------------------------------------	
+class Employee
+	data--> ename, eid, salary, email, image, behavior
+	member function--> insert(), show(), salary() 	
+--------------------------------------------------------------------------------------------------------------	
+class:
+	class is user defined data type which is created to solve real world problem.
+	class is a blueprint, here we actually define our data member and member function, 
+	( it is merely a logical entity)		
+--------------------------------------------------------------------------------------------------------------		
+Feature / characteristics of oops:
+   	1.Class and objects
+   	2.Inheritance
+   	3.Encapsulation
+   	4.Polymorphism (Viva / Interview)
+   	5.Abstraction
+--------------------------------------------------------------------------------------------------------------
