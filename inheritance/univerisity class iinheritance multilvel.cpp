4aa0f216476c0a8becf07d93d771bdc7b9bd0666
Abhay Dixit <dixitshivam534@gmail.com>
2024-08-06 12:32:14 +0530

    university class using multilevel inheritance:

--- inheritance/univerisity class iinheritance multilvel.cpp
+++ inheritance/univerisity class iinheritance multilvel.cpp
@@ -1,61 +1,45 @@
-//WAP to create a university class 
-//having data member:university_name, address, session, number_of_streams,
-//number_of_college_affilated,
-
-//and member functions:
-//parameterized constructor(), 
-//setter(), getter()
-//
-//create a child class College having attribute
-//data members: college_name, address, number_of_students, 
-//
-//create a child class Student having attribute
-//data members: student_name,address, 
+//WAP to create a university class having data member:university_name, address, session, number_of_streams, number_of_college_affilated,
+//and member functions: parameterized constructor(), setter(), getter()
+//create a child class College having attribute data members: college_name, address, number_of_students, 
+//create a child class Student having attribute data members: student_name,address, 
 
 #include<iostream>
 using namespace std;
 
 class University{
-    private:
+    protected:
     string university_name;
-    string address;
-    string session;
-    int number_of_streams;
-    int number_of_college_affilated;
     
     public:
-    University(string u_n,string add, string sess, int n_o_s, int n_o_c_a){
+    University(string u_n){
         university_name=u_n;
-        address=add;
-        session=sess;
-        number_of_streams=n_o_s;
-        number_of_college_affilated=n_o_c_a;
-    }
-    void getter(){
-        cout<<"University Name is:"<< university_name<<endl;
-        cout<<"Address:"<<address<<endl;
-        cout<<"Session"<<session<<endl;
-        cout<<"Number of streams:"<<number_of_streams<<endl;
-        cout<<"Number of college affilated:"<<number_of_college_affilated<<endl;
-    }   
+    } 
 };
 
 class College : public University{
-    public:
-    College(string u_n,string add, string sess, int n_o_s, int n_o_c_a) 
-        : University(u_n, add, sess, n_o_s, n_o_c_a) {}
+	protected:
+	string college_name;
+	public:
+		College(string u_n,string coll_name):University(u_n){
+			college_name=coll_name;
+		}
 };
 
-class Student: public College{
-    public:
-    Student(string u_n,string add, string sess, int n_o_s, int n_o_c_a) 
-        : College(u_n, add, sess, n_o_s, n_o_c_a) {}
+class Student : public College{
+	protected:
+		string student_name;
+	public:
+		Student(string u_n,string coll_name,string stud_name):College(u_n,coll_name){
+			student_name=stud_name;
+		}
+		void display(){
+			cout<<"University Name:"<<university_name<<endl;
+			cout<<"College Name:"<<college_name<<endl;
+			cout<<"Student Name:"<<student_name<<endl;
+		}
 };
 
 int main(){
-    Student obj("RGPV", "Ashram_tiraha", "2023-24", 6, 250);
-    obj.getter();
+	Student obj1("RGPV","UIT-RGPV","Abhay Dixit");
+	obj1.display();	
 }
-
-
-
