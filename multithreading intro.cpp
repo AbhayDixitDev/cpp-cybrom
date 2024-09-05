7ea0d77089cf4b467084999814ed78a0b4fda605
Abhay Dixit <dixitshivam534@gmail.com>
2024-09-05 12:32:32 +0530

    multithreading complete

--- multithreading intro.cpp
+++ multithreading intro.cpp
@@ -1,5 +1,40 @@
-introduction to multithreading
+//introduction to multithreading
+//
+//break the program in  multiple process and process them in parallel way
+//
+//like a thread we can divide single thread into multiple thread
 
-break the program in  multiple process and process them in parallel way
+#include <thread>
+#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+void task1(int n){
+	for (int i=1;i<n;i++){
+		n=n+n*n*n;
+		n++;
+		this_thread::sleep_for(milliseconds(1000));
+	}
+	cout<<"task 1 completed\n";
+}
 
-like a thread we can divide single thread into multiple thread
+void task2(int n){
+	for (int i=1;i<n;i++){
+		n=n+n*n*n;
+		n++;
+		this_thread::sleep_for(milliseconds(1000));
+	}
+	cout<<"task 2 completed\n";
+}
+
+
+int main()
+{
+	thread t1(task1,1000);
+	thread t2(task2,1000);
+	t1.join();
+	t2.join();
+	
+}
+
+
